f8a63945adc120186fa2a658af71448f9b19c749
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-22 15:17:37 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ require 'pry'
 def key_for_min_value(name_hash)
   lowest = 65535
   name_hash.each { |name, value|
-    result = name if value < lowest
+    self.result = name if value < lowest
     lowest = value if value < lowest
   }
   binding.pry
